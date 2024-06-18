d495158c5f07ae4ced7c8c5fe6f5d4469b20e5d1
Paola De Bartolo <paola.debartolo@gmail.com>
2024-06-18 17:56:07 -0300

    chore: add missing license headers

--- src/fc-whatsapp-button.ts
+++ src/fc-whatsapp-button.ts
@@ -1,3 +1,27 @@
+/*-
+ * #%L
+ * @flowingcode/fc-whatsapp-button
+ * %%
+ * Copyright (C) 2022 - 2024 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+/* 
+ * This file incorporates work licensed under MIT. 
+ * Copyright (C) 2021 Mohammed Shah <@smashah> https://github.com/open-wa/whatsapp-button
+ */
 import { css, html, LitElement, PropertyValueMap } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
